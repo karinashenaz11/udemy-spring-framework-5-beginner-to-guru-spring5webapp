1ef5e6c5508422cff9283aa055f06d1ab6ab3b90
karinashenaz11 <karinashenaz@gmail.com>
2020-10-11 17:56:32 +0100

    comments and clean up

--- src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -3,5 +3,6 @@ package guru.springframework.spring5webapp.repositories;
 import guru.springframework.spring5webapp.model.Book;
 import org.springframework.data.repository.CrudRepository;
 
+//Implementation of the repository pattern- eric evans' book - Domain Driven Design book.
 public interface BookRepository extends CrudRepository<Book, Long> {
 }
